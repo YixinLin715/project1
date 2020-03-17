4ed7ef77c683641bc75f89a851461abf1496994b
YixinLin715 <54845763+YixinLin715@users.noreply.github.com>
2020-03-17 19:52:35 +0100

    Update project1.sh

--- project1.sh
+++ project1.sh
@@ -1,13 +1,20 @@
 #date=0317
-awk '{print $1,$2}' GorgorWholeGen.ped   #check the subspecies information of griollas
+#check the subspecies information of gorillas
+awk '{print $1,$2}' GorgorWholeGen.ped  
 
-nano sampleID.txt # picked 4 individuals of each subspecies
-
+# picked 4 individuals of each subspecies(3*4 + 1Ggd)
+nano sampleID.txt 
+# select
+Gbb-Maisha Gbb-Maisha
 Gbb-N010_Turimaso Gbb-N010_Turimaso
-Gbg-9732_Mkubwa Gbg-9732_Mkubwa
+Gbb-Umurimo Gbb-Umurimo
+Gbg-A929_Kaisi Gbg-A929_Kaisi
+Gbg-N011_Pinga Gbg-N011_Pinga
+Gbg-N012_Dunia Gbg-N012_Dunia
 Ggd-B646_Nyango Ggd-B646_Nyango
-Ggg-B647_Anthal Ggg-B647_Anthal
-
+Ggg-A932_Mimi Ggg-A932_Mimi
+Ggg-KB5792_Carolyn Ggg-KB5792_Carolyn
+Ggg-X00108_Abe Ggg-X00108_Abe
 
 #make bed file for whole genome file of all individuals
 plink --file GorgorWholeGen --make-bed --out GorgorWholeGen 
